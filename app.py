27920f280577994d1f15ab8b956b313c9c912168
dhivagargopal <dhivagargopalg@gmail.com>
2026-01-25 12:40:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,31 @@
-from flask import Flask
-import time
+from multiprocessing import Process
 import os
+import app
+import worker
+import data_generator
 
-app = Flask(__name__)
+def run_app():
+    port = int(os.getenv("APP_PORT", 5000))
+    app.app.run(host="0.0.0.0", port=port)
 
-@app.route("/")
-def home():
-    return f"Hello from pod {os.getenv('HOSTNAME')}"
+def run_worker():
+    worker.main()
 
-@app.route("/health")
-def health():
-    return "OK", 200
+def run_data_generator():
+    port = int(os.getenv("DATA_PORT", 6000))
+    data_generator.app.run(host="0.0.0.0", port=port)
 
-@app.route("/cpu")
-def cpu():
-    end = time.time() + 10
-    while time.time() < end:
-        pass
-    return "CPU load generated!"
-    
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    # Create separate processes for each service
+    p1 = Process(target=run_app)
+    p2 = Process(target=run_worker)
+    p3 = Process(target=run_data_generator)
+
+    p1.start()
+    p2.start()
+    p3.start()
+
+    # Wait for all processes to finish (they won’t, services run indefinitely)
+    p1.join()
+    p2.join()
+    p3.join()
